f1a406249c94f53f0fa2701fbb05ba2b173ee6f6
Voc-夜芷冰 <voc.app.programmer@gmail.com>
2025-04-03 17:02:29 +0800

    feat: Added Semi-While-Loop to Query Brightness

--- app/src/main/java/com/example/asm2_solar_game/MainActivity.java
+++ app/src/main/java/com/example/asm2_solar_game/MainActivity.java
@@ -1,4 +1,6 @@
 package com.example.asm2_solar_game;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.widget.TextView;
 import android.content.ContentResolver;
 import android.content.Context;
@@ -13,6 +15,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     private TextView brightness;
+    private final Handler handler = new Handler();
+    private Runnable runnable = () -> {
+
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,10 +31,31 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        // Call the getScreenBrightness method
-        int brightness = getScreenBrightness(this);
-        // Optionally, do something with the brightness value
-        System.out.println("Brightness retrieved in onCreate: " + brightness);
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                // Call the getScreenBrightness method
+                int brightnessValue = getScreenBrightness(MainActivity.this);
+                // Optionally, do something with the brightness value
+                System.out.println("Brightness retrieved in onCreate: " + brightnessValue);
+
+                // Update the TextView with the brightness value
+                brightness.setText("Brightness: " + brightnessValue);
+                // 70% -> do show XXX
+
+                // Re-post the Runnable to run again after 100ms
+                handler.postDelayed(this, 100);
+            }
+        };
+
+        // Start the loop
+        handler.post(runnable);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacks(runnable);
     }
 
     private int getScreenBrightness(Context context) {
